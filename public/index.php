e3f53d4968bbc714471ea2b8a9e8977a47db5212
Marcel-Fassou-28 <marcelfassouhaba28@gmail.com>
2025-03-04 00:12:57 +0000

    Documentation du fichier carrefour

--- public/index.php
+++ public/index.php
@@ -4,11 +4,18 @@ use App\Router;
 
 $router = new Router(dirname(__DIR__) . '/views');
 $router
+
+    /**
+     * Pour les utilisateurs de la plateforme
+     */
     ->get('/', 'home/index', 'accueil')
     ->match('/login', 'login/login', 'login_page')
     ->match('/login/[*:slug]/[*:id]', 'compte/profil', 'login')
     ->get('/login/forget_password', 'Authentification/forget_password', 'forget_password')
     ->get('/login/forget_password/password_recorver/[*:slug]-[i:id]', 'Authentification/password_recorver', 'password_recover')
-    //administration
+
+    /**
+     * Pour l'administration uniquement 
+     */
     ->get('/administration', 'admin/login', 'administration')
     ->run();
