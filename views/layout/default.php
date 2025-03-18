84e22afa95c42bb110663041b20cc3211c7df80a
Marcel-Fassou-28 <marcelfassouhaba28@gmail.com>
2025-03-18 12:30:59 +0000

    Ajout d'un footer

--- views/layout/default.php
+++ views/layout/default.php
@@ -25,7 +25,10 @@
     </nav>
     <div class="toggle">&times;</div>
     <div class="toggle-open">&#9776;</div>
-    <?= $content ?>
+    <main class="col-13">
+        <?= $content ?>
+    </main>
+    <footer></footer>
     <script src="/js/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
