190f40926c1a3429d187a43df99c08e4ce130a6b
NGONORMALIA <maliangonor00@gamil.com>
2025-04-06 07:55:45 +0100

    hj

--- views/utilisateur/profile.php
+++ views/utilisateur/profile.php
@@ -9,13 +9,16 @@ function encodindCIN($cin) {
 
  $sql = "SELECT * FROM utilisateur";
  $info = [];
+ var_dump($_SESSION);
  foreach($pdo->query($sql) as $row){
+   
     if ($_SESSION['id_user'] === $row['cin']){ //J'ai remplacé l'id par cin, car la variale de session id_user est le cin
         foreach($row as $col=>$val){
             if (!is_integer($col)){
                $info[$col] = $val; 
             }  
         }
+        var_dump($info);
     }
  }
 
